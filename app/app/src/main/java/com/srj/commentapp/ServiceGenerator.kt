0489637eecc2fd8cc6e6bb175892eef5e75ab184
SRJ_ranjan <sudhanshuwithyoualll@gmail.com>
2022-02-11 22:55:43 +0530

    Retrofit service
    Signed-off-by: SRJ_ranjan <sudhanshuwithyoualll@gmail.com>

---
+++ app/app/src/main/java/com/srj/commentapp/ServiceGenerator.kt
@@ -0,0 +1,24 @@
+package com.srj.commentapp
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ServiceGenerator {
+    val gson: Gson
+        get() = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+    val baseUrl: String
+        get() = "https://comments-app-api.deta.dev/"
+    val retrofit: Retrofit
+        get() = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+}
